4b0890b7761a688999c6a224174c34c391d80914
Ezequiel <ezemgaray@gmail.com>
2020-11-21 02:33:46 +0100

    Add private Profile section

---
+++ src/pages/PrivateRoutes/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Navigation} from '../../components'
+
+export const Profile = () => (
+  <div className="container">
+    <Navigation />
+    <h1 style={{color:'red'}}>Profile</h1>
+  </div>
+)
