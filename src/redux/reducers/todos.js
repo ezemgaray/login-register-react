485e6227487f6a37b2df728405c1587d69e67433
Ezequiel <ezemgaray@gmail.com>
2020-11-21 03:22:35 +0100

    Add 'todos' reducer to test combine reducers

---
+++ src/redux/reducers/todos.js
@@ -0,0 +1,11 @@
+
+const initialState = {
+    test: 'test combine reducers',
+    message: 'May can be an initial global state'
+}
+
+const todos = (state = initialState, action) => {
+  return state
+}
+
+export default todos
