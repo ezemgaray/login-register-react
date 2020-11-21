6f314fcd87d0c0488b313576993549d52978e771
Ezequiel <ezemgaray@gmail.com>
2020-11-21 02:04:31 +0100

    Add Home Page View

---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Navigation} from '../components'
+
+export const HomePage = () => (
+  <div className="container">
+    <Navigation />
+    <h1 style={{color:'red'}}>Home Page</h1>
+  </div>
+)
