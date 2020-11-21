6f90e0c184b75b7c05b96c88018c81c72dcdc463
Ezequiel <ezemgaray@gmail.com>
2020-11-21 02:32:01 +0100

    Add User Panel

---
+++ src/pages/PrivateRoutes/Panel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Navigation} from '../../components'
+
+export const Panel = () => (
+  <div className="container">
+    <Navigation />
+    <h1 style={{color:'red'}}>Home Panel</h1>
+  </div>
+)
