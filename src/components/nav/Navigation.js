8185d5ddd4c0f46f50feae2ba2e77e23034c5fa0
Ezequiel <ezemgaray@gmail.com>
2020-11-22 09:00:33 +0100

    Refactor Navigation component - Add NavLink

--- src/components/nav/Navigation.js
+++ src/components/nav/Navigation.js
@@ -1,5 +1,5 @@
 import React                      from 'react'
-import { Link, withRouter }       from 'react-router-dom'
+import { NavLink, withRouter }    from 'react-router-dom'
 
 const Nav = ({
   currentUserState: { user } = {},
@@ -9,37 +9,54 @@ const Nav = ({
   return (
     <div className="">
       <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Navbar</Link>
+        <NavLink className="navbar-brand" to="/">Navbar</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                  <Link className="nav-link" to="/">Home</Link>
+              <li className="nav-item">
+                  <NavLink
+                    exact
+                    to="/"
+                    className="nav-link"
+                  >
+                    Home
+                  </NavLink>
               </li>
               <li className="nav-item">
-                  <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                  <NavLink
+                    exact
+                    to="/dashboard"
+                    className="nav-link"
+                  >
+                    Dashboard
+                  </NavLink>
               </li>
               <li className="nav-item">
-                  <Link className="nav-link" to="/dashboard/profile">Profile</Link>
+                  <NavLink
+                    to="/dashboard/profile"
+                    className="nav-link"
+                  >
+                    Profile
+                  </NavLink>
               </li>
           </ul>
           <ul className="navbar-nav">
-              <li className="nav-item">
-                {
-                  !user
-                    ? <Link className="nav-link" to="/login">Login</Link>
-                    : <button className="btn btn-secondary" onClick={ () => logout() }>Logout</button>
-                }
-              </li>
-              {
-                !user
-                  ? <li className="nav-item">
-                      <Link className="nav-link" to="/register">Register</Link>
+            {
+              !user
+                ? (
+                  <>
+                    <li className="nav-item">
+                      <NavLink to="/login" className="nav-link">Login</NavLink>
                     </li>
-                  : ''
-              }
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/register">Register</NavLink>
+                    </li>
+                  </>
+                )
+                : <button className="btn btn-secondary" onClick={ () => logout() }>Logout</button>
+            }
           </ul>
         </div>
       </nav>
