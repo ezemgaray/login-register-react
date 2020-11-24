215d8066a5feb7b8ca69babf28af5dceff1ed79e
Ezequiel <ezemgaray@gmail.com>
2020-11-24 14:00:04 +0100

    Refactor LoadUserContainer - remove mapStateToProps

--- src/redux/containers/components/LoadUserContainer.js
+++ src/redux/containers/components/LoadUserContainer.js
@@ -1,33 +1,34 @@
-import { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { resetStateAction } from '../../actions/user'
-import { getUserToken, deleteUserToken } from '../../../utils/auth'
-import api from '../../../services/api'
+import { useEffect }                      from 'react'
+import { connect }                        from 'react-redux'
+import { resetStateAction }               from '../../actions/user'
+import { getUserToken, deleteUserToken }  from '../../../utils/auth'
+import api                                from '../../../services/api'
 
 
 function LoadUser({
-  state,
-  currentUserState: { user } = {},
   resetState
 }) {
   useEffect(()=>{
+    /**
+     * Check user logged in. 
+     * Check if the token is correck and return user data.
+     */
     const checkLoggedIn = async () => {
       const token = getUserToken()
-      if(!token) return
-      
-      try {
-
-        const user = await api.user.whoami(token)
-          .then(({data}) => data)
-          .catch( error => {
-            deleteUserToken()
-          })
-          if(user)
-            resetState('user', {...user, token})
-      } catch (error) {
-        
+      if(!token) return // If there are no toke, continue
+      const user = await api.user.whoami(token)
+      .then(({data}) => data)
+      .catch( _ => {
+        deleteUserToken() // When the token is wron, remove the token
+      })
+      // Update state only if the token is correct
+      if(user){
+        resetState('user', {
+          ...user,
+          token
+        })
+        resetState('isLoggedIn', true)
       }
-
     }
     checkLoggedIn()
   }, [resetState])
@@ -35,13 +36,8 @@ function LoadUser({
   return (<></>)
 }
 
-const mapStateToProps = (state) => ({
-  currentUserState: state.user,
-  state: state
-})
-
 const mapDispatchToProps = (dispatch) => ({
   resetState: (property, value) => dispatch(resetStateAction(property, value)),
 })
 
-export const LoadUserContainer = connect(mapStateToProps, mapDispatchToProps)(LoadUser)
+export const LoadUserContainer = connect(null, mapDispatchToProps)(LoadUser)
